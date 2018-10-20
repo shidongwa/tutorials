11a3d5ba790f6746f884a1fc149e387d601b1c3e
stone.shi <10189895@qq.com>
2018-10-21 06:53:13 +0800

    compare orika and mapStruct performance

--- performance-tests/src/test/java/com/baeldung/performancetests/benchmark/MappingFrameworksPerformance.java
+++ performance-tests/src/test/java/com/baeldung/performancetests/benchmark/MappingFrameworksPerformance.java
@@ -1,35 +1,10 @@
 package com.baeldung.performancetests.benchmark;
 
-import com.baeldung.performancetests.dozer.DozerConverter;
-import com.baeldung.performancetests.jmapper.JMapperConverter;
 import com.baeldung.performancetests.mapstruct.MapStructConverter;
-import com.baeldung.performancetests.model.destination.DestinationCode;
 import com.baeldung.performancetests.model.destination.Order;
-import com.baeldung.performancetests.model.source.AccountStatus;
-import com.baeldung.performancetests.model.source.Address;
-import com.baeldung.performancetests.model.source.DeliveryData;
-import com.baeldung.performancetests.model.source.Discount;
-import com.baeldung.performancetests.model.source.OrderStatus;
-import com.baeldung.performancetests.model.source.PaymentType;
-import com.baeldung.performancetests.model.source.Product;
-import com.baeldung.performancetests.model.source.RefundPolicy;
-import com.baeldung.performancetests.model.source.Review;
-import com.baeldung.performancetests.model.source.Shop;
-import com.baeldung.performancetests.model.source.SourceCode;
-import com.baeldung.performancetests.model.source.SourceOrder;
-import com.baeldung.performancetests.model.source.User;
-import com.baeldung.performancetests.modelmapper.ModelMapperConverter;
+import com.baeldung.performancetests.model.source.*;
 import com.baeldung.performancetests.orika.OrikaConverter;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Group;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Scope;
-import org.openjdk.jmh.annotations.Setup;
-import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.*;
 import org.openjdk.jmh.runner.RunnerException;
 
 import java.io.IOException;
@@ -40,18 +15,19 @@ import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-@Fork(value = 1, warmups = 5)
+@Fork(value = 1)
+@Warmup(iterations = 3)
 @OutputTimeUnit(TimeUnit.MILLISECONDS)
-@BenchmarkMode(Mode.All)
+@BenchmarkMode(Mode.Throughput)
 @Measurement(iterations = 5)
 @State(Scope.Group)
 public class MappingFrameworksPerformance {
     private SourceOrder sourceOrder = null;
     private SourceCode sourceCode = null;
     private static final OrikaConverter ORIKA_CONVERTER = new OrikaConverter();
-    private static final JMapperConverter JMAPPER_CONVERTER = new JMapperConverter();
+/*    private static final JMapperConverter JMAPPER_CONVERTER = new JMapperConverter();
     private static final ModelMapperConverter MODEL_MAPPER_CONVERTER = new ModelMapperConverter();
-    private static final DozerConverter DOZER_CONVERTER = new DozerConverter();
+    private static final DozerConverter DOZER_CONVERTER = new DozerConverter();*/
 
     @Setup
     public void setUp() {
@@ -107,7 +83,7 @@ public class MappingFrameworksPerformance {
         return ORIKA_CONVERTER.convert(sourceOrder);
     }
 
-    @Benchmark
+/*    @Benchmark
     @Group("realLifeTest")
     public Order jmapperRealLifeBenchmark() {
         return JMAPPER_CONVERTER.convert(sourceOrder);
@@ -123,7 +99,7 @@ public class MappingFrameworksPerformance {
     @Group("realLifeTest")
     public Order dozerMapperRealLifeBenchmark() {
         return DOZER_CONVERTER.convert(sourceOrder);
-    }
+    }*/
 
     @Benchmark
     @Group("realLifeTest")
@@ -131,7 +107,7 @@ public class MappingFrameworksPerformance {
         return MapStructConverter.MAPPER.convert(sourceOrder);
     }
 
-    @Benchmark
+   /* @Benchmark
     @Group("simpleTest")
     public DestinationCode orikaMapperSimpleBenchmark() {
         return ORIKA_CONVERTER.convert(sourceCode);
@@ -159,5 +135,5 @@ public class MappingFrameworksPerformance {
     @Group("simpleTest")
     public DestinationCode mapStructMapperSimpleBenchmark() {
         return MapStructConverter.MAPPER.convert(sourceCode);
-    }
+    }*/
 }
